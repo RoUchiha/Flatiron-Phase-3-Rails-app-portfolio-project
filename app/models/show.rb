296073a1cda122f5e18cbcedefa7c1cd310527e0
RoUchiha <swaggsikh@gmail.com>
2022-02-10 01:36:41 -0600

    added 'titleize' to all show attributes.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,14 +1,16 @@
 class Show < ApplicationRecord
 
     validates :name, presence: true
+    after_validation { self.name = self.name.titleize }
     validates :genre, presence: true
+    after_validation { self.genre = self.genre.titleize }
     validates :episodes, presence: true, numericality: { only_integer: true }
     before_validation { self.status = self.status.downcase }
     validates :status, presence: true, inclusion: { in: ["watching", "plan to watch", "watched"]}
-    after_validation { self.status = self.status.capitalize }
+    after_validation { self.status = self.status.titleize }
 
     belongs_to :user 
-    has_many :comments
+    has_one :comment
     has_one :rating
 
 end
