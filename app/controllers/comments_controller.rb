0aab12fdec7f77dec7963f7d4a49a5fd3a145e95
RoUchiha <swaggsikh@gmail.com>
2022-02-11 20:44:04 -0600

    new Comment create action done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,20 @@
 class CommentsController < ApplicationController
 
+    helper_method :logged_in?, :current_user
+
+
     def new
-        @show = Show.find_by(id: params[:show_id])
-        @comment = Comment.new(show_id: params[:show_id])
         @user = current_user 
+        @show = Show.find_by(id: params[:show_id])
+        @comment = Comment.new(show_id: params[:show_id], user_id: current_user.id)
+       
     end
 
     def create 
+        @user = current_user
         @show = Show.find_by(id: params[:show_id])
         @comment = Comment.new(comment_params)
-        @user = current_user
+       
 
         if @comment.save 
             redirect_to show_path(@show)
